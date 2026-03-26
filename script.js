83f6cec6f5298d46ff634b82fcc58b2580e4678b
yashashree0123 <117700343+yashashree0123@users.noreply.github.com>
2026-03-26 03:43:55 +0000

    added code for all of the functions that i did in class as it didnrt save

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 // ----- Functions to implement -----
 
 // 1) myFunc(): persistent counter
+let count=0;
+
+function onMyFuncClick(){
+  count++;
+  return count;
+}
 
 // 2) getRandomNum(max): 1..max int or 0 if invalid
+function getRandomNum(max){
+  max=parselnt(max);
+  if(isNaN(max) || max<1) return0;
+  return Math.floor(Math.random()*max)+1;
+}
 
 // 3) myAdder(x, y): numeric sum
-
+function myAddre(x,y){
+  x=parselnt(x);
+  y=parselnt(y);
+  ADD=x+y;
+  return ADD;
+}
 // 4) distance(x1, y1, x2, y2): Euclidean distance
-
+function distance(x1, y1, x2, y2){
+  return Math.sqrt((x2-x11)**2 +(y2-y1)**2);
+}
 // 5) quadratic(a, b, c): roots of ax^2 + bx + c = 0
-
+function quadratic(a, b,c){
+  let disc= b*b-4*a*c;
+  if(disc>0){
+    let r1=(-b + Math.sqrt(disc))/(2*a);
+    let r2=(-b - Math.sqrt(disc))/(2*a);
+    return[-b/(2*a)];
+        } else if (disc === 0) {
+        return [-b / (2 * a)];
+    } else {
+        let real = (-b / (2 * a));
+        let imag = (Math.sqrt(-disc) / (2 * a));
+        return [real + "+" + imag + "i", real + "-" + imag + "i"];
+  }
+}
 
 // ----- Helpers -----
 function $(id) { return document.getElementById(id); }
